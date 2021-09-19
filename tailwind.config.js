07dbcdac7223d3157aad021795b3cdc17f44b9db
Igor Chodelka <igor.chodelka@gmail.com>
2021-09-19 19:59:23 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const purge = process.env.NODE_ENV === 'production' ? true : false;
+module.exports = {
+  purge: {
+    enabled: purge,
+    content: ['./public/**/*.html','./public/**/*.js',]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
